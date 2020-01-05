7ac77512d78e63b57496624da139a8d4f927a7e9
Mikko Korpela <mikko.korpela@gmail.com>
2020-01-05 17:19:10 +0200

    case

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(join(CURDIR, 'README.rst')) as f:
     LONG_DESCRIPTION = f.read()
 
 setup(name='robotframework-expects',
-      version='0.0.1',
+      version='0.1.1',
       description=desc,
       long_description=LONG_DESCRIPTION,
       author=name,
